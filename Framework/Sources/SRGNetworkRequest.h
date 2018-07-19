b11bb992604f36acc7c03d3b9b2b4243c729e098
Samuel Défago <defagos@gmail.com>
2018-07-19 13:42:39 +0200

    Add options

--- Framework/Sources/SRGNetworkRequest.h
+++ Framework/Sources/SRGNetworkRequest.h
@@ -8,12 +8,17 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+typedef NS_OPTIONS(NSUInteger, SRGNetworkRequestOptions) {
+    SRGNetworkRequestOptionIgnoreCancellationErrors = (1UL << 0),
+    SRGNetworkRequestOptionIgnoreHTTPErrors = (1UL << 0),
+};
+
 @interface SRGNetworkRequest : NSObject
 
-- (instancetype)initWithRequest:(NSURLRequest *)request session:(NSURLSession *)session completionBlock:(void (^)(NSData * _Nullable data, NSError * _Nullable error))completionBlock NS_DESIGNATED_INITIALIZER;
+- (instancetype)initWithRequest:(NSURLRequest *)request session:(NSURLSession *)session options:(SRGNetworkRequestOptions)options completionBlock:(void (^)(NSData * _Nullable data, NSURLResponse * _Nullable response,  NSError * _Nullable error))completionBlock NS_DESIGNATED_INITIALIZER;
 
-- (instancetype)initWithJSONDictionaryRequest:(NSURLRequest *)request session:(NSURLSession *)session completionBlock:(void (^)(NSDictionary * _Nullable JSONDictionary, NSError * _Nullable error))completionBlock;
-- (instancetype)initWithJSONArrayRequest:(NSURLRequest *)request session:(NSURLSession *)session completionBlock:(void (^)(NSArray * _Nullable JSONArray, NSError * _Nullable error))completionBlock;
+- (instancetype)initWithJSONDictionaryRequest:(NSURLRequest *)request session:(NSURLSession *)session options:(SRGNetworkRequestOptions)options completionBlock:(void (^)(NSDictionary * _Nullable JSONDictionary, NSURLResponse * _Nullable response, NSError * _Nullable error))completionBlock;
+- (instancetype)initWithJSONArrayRequest:(NSURLRequest *)request session:(NSURLSession *)session options:(SRGNetworkRequestOptions)options completionBlock:(void (^)(NSArray * _Nullable JSONArray, NSURLResponse * _Nullable response, NSError * _Nullable error))completionBlock;
 
 - (void)resume;
 - (void)cancel;
--- Framework/Sources/SRGNetworkRequest.m
+++ Framework/Sources/SRGNetworkRequest.m
@@ -20,86 +20,89 @@
 
 #pragma marl Object lifecycle
 
-- (instancetype)initWithRequest:(NSURLRequest *)request session:(NSURLSession *)session completionBlock:(void (^)(NSData * _Nullable data, NSError * _Nullable error))completionBlock;
+- (instancetype)initWithRequest:(NSURLRequest *)request session:(NSURLSession *)session options:(SRGNetworkRequestOptions)options completionBlock:(void (^)(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error))completionBlock;
 {
     if (self = [super init]) {
         self.sessionTask = [session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
             if (error) {
                 if ([error.domain isEqualToString:NSURLErrorDomain] && error.code == NSURLErrorCancelled) {
-                    return;
+                    if ((options & SRGNetworkRequestOptionIgnoreCancellationErrors) == 0) {
+                        return;
+                    }
                 }
-                else {
-                    completionBlock(nil, error);
-                }
+                
+                completionBlock(nil, response, error);
                 return;
             }
             
-            if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
-                NSHTTPURLResponse *HTTPURLResponse = (NSHTTPURLResponse *)response;
-                NSInteger HTTPStatusCode = HTTPURLResponse.statusCode;
-                
-                // Properly handle HTTP error codes >= 400 as real errors
-                if (HTTPStatusCode >= 400) {
-                    NSError *HTTPError = [NSError errorWithDomain:SRGNetworkErrorDomain
-                                                             code:SRGNetworkErrorHTTP
-                                                         userInfo:@{ NSLocalizedDescriptionKey : [NSHTTPURLResponse srg_localizedStringForStatusCode:HTTPStatusCode],
-                                                                     NSURLErrorKey : response.URL,
-                                                                     SRGNetworkHTTPStatusCodeKey : @(HTTPStatusCode) }];
-                    completionBlock(nil, HTTPError);
-                    return;
+            if ((options & SRGNetworkRequestOptionIgnoreHTTPErrors) == 0) {
+                if ([response isKindOfClass:[NSHTTPURLResponse class]]) {
+                    NSHTTPURLResponse *HTTPURLResponse = (NSHTTPURLResponse *)response;
+                    NSInteger HTTPStatusCode = HTTPURLResponse.statusCode;
+                    
+                    // Properly handle HTTP error codes >= 400 as real errors
+                    if (HTTPStatusCode >= 400) {
+                        NSError *HTTPError = [NSError errorWithDomain:SRGNetworkErrorDomain
+                                                                 code:SRGNetworkErrorHTTP
+                                                             userInfo:@{ NSLocalizedDescriptionKey : [NSHTTPURLResponse srg_localizedStringForStatusCode:HTTPStatusCode],
+                                                                         NSURLErrorKey : response.URL,
+                                                                         SRGNetworkHTTPStatusCodeKey : @(HTTPStatusCode) }];
+                        completionBlock(nil, response, HTTPError);
+                        return;
+                    }
                 }
             }
             
-            completionBlock(data, nil);
+            completionBlock(data, response, nil);
         }];
     }
     return self;
 }
 
-- (instancetype)initWithJSONDictionaryRequest:(NSURLRequest *)request session:(NSURLSession *)session completionBlock:(void (^)(NSDictionary * _Nullable, NSError * _Nullable))completionBlock
+- (instancetype)initWithJSONDictionaryRequest:(NSURLRequest *)request session:(NSURLSession *)session options:(SRGNetworkRequestOptions)options completionBlock:(void (^)(NSDictionary * _Nullable, NSURLResponse * _Nullable response, NSError * _Nullable))completionBlock
 {
-    return [self initWithRequest:request session:session completionBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+    return [self initWithRequest:request session:session options:options completionBlock:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (error) {
-            completionBlock(nil, error);
+            completionBlock(nil, response, error);
             return;
         }
         
         id JSONDictionary = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
         if (! JSONDictionary || ! [JSONDictionary isKindOfClass:[NSDictionary class]]) {
-            completionBlock(nil, [NSError errorWithDomain:SRGNetworkErrorDomain
-                                                     code:SRGNetworkErrorInvalidData
-                                                 userInfo:@{ NSLocalizedDescriptionKey : SRGNetworkLocalizedString(@"The data is invalid.", @"The error message when the response from IL server is incorrect.") }]);
+            completionBlock(nil, response, [NSError errorWithDomain:SRGNetworkErrorDomain
+                                                               code:SRGNetworkErrorInvalidData
+                                                           userInfo:@{ NSLocalizedDescriptionKey : SRGNetworkLocalizedString(@"The data is invalid.", @"The error message when the response from IL server is incorrect.") }]);
             return;
         }
         
-        completionBlock(JSONDictionary, nil);
+        completionBlock(JSONDictionary, response, nil);
     }];
 }
 
-- (instancetype)initWithJSONArrayRequest:(NSURLRequest *)request session:(NSURLSession *)session completionBlock:(void (^)(NSArray * _Nullable, NSError * _Nullable))completionBlock
+- (instancetype)initWithJSONArrayRequest:(NSURLRequest *)request session:(NSURLSession *)session options:(SRGNetworkRequestOptions)options completionBlock:(void (^)(NSArray * _Nullable, NSURLResponse * _Nullable response, NSError * _Nullable))completionBlock
 {
-    return [self initWithRequest:request session:session completionBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+    return [self initWithRequest:request session:session options:options completionBlock:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         if (error) {
-            completionBlock(nil, error);
+            completionBlock(nil, response, error);
             return;
         }
         
         id JSONArray = [NSJSONSerialization JSONObjectWithData:data options:0 error:NULL];
         if (! JSONArray || ! [JSONArray isKindOfClass:[NSArray class]]) {
-            completionBlock(nil, [NSError errorWithDomain:SRGNetworkErrorDomain
-                                                     code:SRGNetworkErrorInvalidData
-                                                 userInfo:@{ NSLocalizedDescriptionKey : SRGNetworkLocalizedString(@"The data is invalid.", @"The error message when the response from IL server is incorrect.") }]);
+            completionBlock(nil, response, [NSError errorWithDomain:SRGNetworkErrorDomain
+                                                               code:SRGNetworkErrorInvalidData
+                                                           userInfo:@{ NSLocalizedDescriptionKey : SRGNetworkLocalizedString(@"The data is invalid.", @"The error message when the response from IL server is incorrect.") }]);
             return;
         }
         
-        completionBlock(JSONArray, nil);
+        completionBlock(JSONArray, response, nil);
     }];
 }
 
 - (instancetype)init
 {
     [self doesNotRecognizeSelector:_cmd];
-    return [self initWithRequest:[NSURLRequest new] session:[NSURLSession new] completionBlock:^(NSData * _Nullable data, NSError * _Nullable error) {
+    return [self initWithRequest:[NSURLRequest new] session:[NSURLSession new] options:0 completionBlock:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
         // Nothing
     }];
 }
