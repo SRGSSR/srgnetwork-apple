bf81a332fdb6b66e1463c45ae28750d8ba1cc869
Samuel Défago <defagos@gmail.com>
2018-07-20 08:57:08 +0200

    Add test target

---
+++ Tests/Sources/SRGNetworkRequestTestCase.m
@@ -0,0 +1,16 @@
+//
+//  Copyright (c) SRG SSR. All rights reserved.
+//
+//  License information is available from the LICENSE file.
+//
+
+#import <SRGNetwork/SRGNetwork.h>
+#import <XCTest/XCTest.h>
+
+@interface SRGNetworkRequestTestCase : XCTestCase
+
+@end
+
+@implementation SRGNetworkRequestTestCase
+
+@end
