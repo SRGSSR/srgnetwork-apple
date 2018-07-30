872611789a14eb12256ef19421c8868652806f9e
Samuel Défago <defagos@gmail.com>
2018-07-30 15:53:08 +0200

    Fix build

--- Framework/Sources/Categories/NSBundle+SRGNetwork.m
+++ Framework/Sources/Categories/NSBundle+SRGNetwork.m
@@ -18,10 +18,10 @@
     static dispatch_once_t s_once;
     dispatch_once(&s_once, ^{
         NSString *bundlePath = [[NSBundle bundleForClass:[SRGNetworkRequest class]].bundlePath stringByAppendingPathComponent:@"SRGNetwork.bundle"];
-        bundle = [NSBundle bundleWithPath:bundlePath];
-        NSAssert(bundle, @"Please add SRGNetwork.bundle to your project resources");
+        s_bundle = [NSBundle bundleWithPath:bundlePath];
+        NSAssert(s_bundle, @"Please add SRGNetwork.bundle to your project resources");
     });
-    return bundle;
+    return s_bundle;
 }
 
 @end
