de00ddbb88d6c6f5e31b0d048b4a9101684d7633
Samuel Défago <defagos@gmail.com>
2019-02-20 21:16:12 +0100

    Add network error translation helper

--- Framework/Sources/Categories/NSHTTPURLResponse+SRGNetwork.h
+++ Framework/Sources/Categories/NSHTTPURLResponse+SRGNetwork.h
@@ -11,6 +11,14 @@ NS_ASSUME_NONNULL_BEGIN
 @interface NSHTTPURLResponse (SRGNetwork)
 
 /**
+ *  Returns a capitalized localized string for the specified `NSURLErrorDomain` error code. If the error code has no match
+ *  in the domain, the method returns a generic message.
+ *
+ *  @discussion This method fixes incorrectly non-translated error descriptions in `NSURLErrorDomain` errors.
+ */
++ (NSString *)srg_localizedStringForURLErrorCode:(NSInteger)errorCode;
+
+/**
  *  Returns a capitalized localized string for the specified HTTP status code.
  *
  *  @discussion This method is a fix for the buggy +localizedStringForStatusCode: public method. See
